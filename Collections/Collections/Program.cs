38c6cf202ce5514125e29cd9e1b3c3f7fbd226f8
Aseka <grooves97@gmail.com>
2019-03-11 18:52:02 +0600

    The List collection
    List Collection home  work is done

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>(10) { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int sum = 0;
+            int i = 0;
+            while (i < list.Capacity)
+            {
+                if (i % 2 == 0) sum += list[i];
+                ++i;
+            }
+            Console.WriteLine($"Summ of elements with = {sum}");
+
+            list.RemoveAt(list.IndexOf(list.Max()));
+
+            Console.WriteLine($"Second maximum = {list.Max()}");
+            Console.ReadLine();
+        }
+    }
+}
